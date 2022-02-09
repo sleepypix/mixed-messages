cb5945874cf725a3bdb1f723e3637bde1ea722a4
sleepypix <reza.rahmat.on@gmail.com>
2022-02-09 15:09:31 +0300

    Random message script first draft

--- jcript_mm.js
+++ jcript_mm.js
@@ -1,1 +1,12 @@
-console.log(`let us get started`)
+
+const message = {
+    order: ['must', 'should', 'could', 'should never'],
+    who: ['You', 'Mark','Sekine', 'An alien'],
+    what: ['paint', 'sing at', 'jump over', 'dance around', 'kiss'],
+    where: ['the Eiffel Tower.', 'the Pyramids.', 'a dog kennel.', 'Mount Everest.']
+};
+let randomize = (array) => {
+    let i = Math.floor(Math.random() * array.length);
+    return array[i];
+};
+console.log(randomize(message.who), randomize(message.order), randomize(message.what), randomize(message.where));
