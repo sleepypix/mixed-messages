bfd1b6e330f4fbdc7c9a0d8ae72f761c9718b6fc
sleepypix <reza.rahmat.on@gmail.com>
2022-02-09 23:09:18 +0300

    Finalized project

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# mixed-messages
-A script that generates random advice when you click on the 'get advice' button
+# mixed-messages = Guru Advice
+## What does it do
+A script that generates __silly__ advice from randomly generated sentence blocks.
+
+## What is it for
+It is a project for a Codecademy course
+
+## Inspiration
+A party game I once played. I think it was called who - what - where.
+
+## Where can I see it?
+Here - https://sleepypix.github.io/mixed-messages/
--- jcript_mm.js
+++ jcript_mm.js
@@ -1,16 +1,22 @@
-
+// Storage for the sentence blocks
 const message = {
     order: ['must', 'should', 'could', 'should never'],
-    who: ['You', 'Mark','Sekine', 'An alien'],
-    what: ['paint', 'sing at', 'jump over', 'dance around', 'kiss'],
-    where: ['the Eiffel Tower.', 'the Pyramids.', 'a dog kennel.', 'Mount Everest.']
+    who: ['You', 'Mark Zuckerberg', 'Arnold Schwarzenegger', 'An alien', 
+    'Your boss', 'John Wick', 'Donald Duck'],
+    what: ['paint', 'sing for', 'jump over', 'dance around', 'kiss', 
+    'write a poem about', 'kick', 'hug', 'skip over'],
+    where: ['the Eiffel Tower.', 'the Pyramids.', 'a dog kennel.', 
+    'Mount Everest.', 'a camel', 'the supermarket cashier']
 };
+// Function to generate a random phrase 
 let randomize = (array) => {
     let i = Math.floor(Math.random() * array.length);
     return array[i];
 };
+// Message text block and button
 const messageOutput = document.getElementById('message');
 const adviceButton = document.getElementById('get-advice');
+// Generating random sentemce after clicking the button
 adviceButton.addEventListener( 'click', () => {
     let outputText = (randomize(message.who)+ ' ' + randomize(message.order)+ ' ' + randomize(message.what)+ ' ' + randomize(message.where));
 
